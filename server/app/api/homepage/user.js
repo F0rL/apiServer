840d8c9a0d63ee01f8439a9958b7379a85f907be
F0rl <F0rL@users.noreply.github.com>
2019-11-13 02:57:13 +0800

    登录返回token

--- server/app/api/homepage/user.js
+++ server/app/api/homepage/user.js
@@ -1,34 +1,32 @@
-const Router = require('koa-router');
-
-const { User } = require('@app/model/User');
-const { Success, AuthFailed } = require('@root/core/httpCode');
-const { ParameterException } = require('@root/core/httpCode');
-const { generateToken } = require('@root/utils/setToken');
-const { Auth } = require('@root/middleware/auth');
+const Router = require('koa-router')
+const { Op } = require('sequelize')
+const { User } = require('@app/model/User')
+const { generateToken } = require('@root/utils/setToken')
+const { Auth } = require('@root/middleware/auth')
 
 const router = new Router({
   prefix: '/user'
-});
+})
 
 // POST
 // 注册
 // 必须账号密码昵称
 router.post('/register', async (ctx, next) => {
-  const v = ctx.request.body;
-  console.log(v);
+  const v = ctx.request.body
+  console.log(v)
   const user = {
     email: v.email,
     password: v.password,
     nickname: v.nickname
-  };
-  const msg = await User.create(user);
-  const token = generateToken(msg.id, Auth.USER);
+  }
+  const msg = await User.create(user)
+  const token = generateToken(msg.id, Auth.USER)
   ctx.body = {
     code: 200,
     token,
     msg
-  };
-});
+  }
+})
 
 // POST
 // 登录
@@ -36,16 +34,24 @@ router.post('/register', async (ctx, next) => {
 // 预留第三方wx登录
 // 参数 account(email, mobile) secret type
 router.post('/login', async (ctx, next) => {
-  const v = ctx.request.body;
+  const v = ctx.request.body
   const user = await User.verifyEmailOrPhone(
     v.account,
     v.password,
     parseInt(v.type)
-  );
+  )
+  const userInfo = await User.findOne({
+    where: {
+      [Op.or]: [{ email: v.account }, { phone: v.account }]
+      // email: v.account
+    }
+  })
+  const token = generateToken(userInfo.id, Auth.USER)
   ctx.body = {
     code: 200,
-    user
-  };
-});
+    user,
+    token
+  }
+})
 
-module.exports = router;
+module.exports = router
