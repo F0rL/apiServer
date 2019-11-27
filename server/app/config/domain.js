ebd78209527be1e1a2777b124a80dde850e0030a
F0rl <F0rL@users.noreply.github.com>
2019-11-27 14:41:03 +0800

    修改为线上地址

--- server/app/config/domain.js
+++ server/app/config/domain.js
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
   // 后台
   apiDomain = 'http://localhost:3000'
   // 静态资源nginx
-  staticDomain = 'http://localhost:4000'
+  // staticDomain = 'http://localhost:4000'
+  staticDomain = 'http://static.forl.fun'
   // 模拟获取ip，此为本机ip
   devClientIp = '115.175.217.236'
 } else {
--- server/app/tools/home/helper.js
+++ server/app/tools/home/helper.js
@@ -3,19 +3,19 @@ const { hpStaticDomain } = require('@app/config/hpConfig')
 const imgUrl = [
   {
     id: 1,
-    url: `${hpStaticDomain}/bg/BG_1.jpg`
-  },
-  {
-    id: 2,
-    url: `${hpStaticDomain}/bg/BG_2.jpg`
-  },
-  {
-    id: 2,
     url: `${hpStaticDomain}/bg/BG_3.png`
   },
   {
     id: 2,
     url: `${hpStaticDomain}/bg/BG_4.png`
+  },
+  {
+    id: 2,
+    url: `${hpStaticDomain}/bg/BG_1.jpg`
+  },
+  {
+    id: 2,
+    url: `${hpStaticDomain}/bg/BG_2.jpg`
   },
   {
     id: 2,
